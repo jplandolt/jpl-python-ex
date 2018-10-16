1204a44f75169e2491867293a8456d6db7715a36
Paul Landolt <paul.landolt@cgi.com>
2018-10-16 13:43:57 -0400

    start

---
+++ simple.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+#
+# Simple python thing
+
+
+import os
+import io
+import sys
+
+# main function
+def main (argv=sys.argv[1:]):
+    print("in function - main")
+
+    return
+
+# end function main
+
+# main loop
+if __name__ == '__main__':
+    main()
+
